748c4e5a9fa139d6d0b4e78ef3e34caf84e7bc75
RabinPhaiju <rabinphaiju15@gmail.com>
2021-01-31 10:59:27 +0545

    dijkstra shortest path

---
+++ src/PathfindingVisualizer/Node/Node.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./Node.css";
+
+const node = ({ col, isFinish, isStart, isWall, onMouseDown, onMouseEnter, onMouseUp, row }) => {
+  const extraClassName = isFinish
+    ? "node-finish"
+    : isStart
+    ? "node-start"
+    : isWall
+    ? "node-wall"
+    : "";
+
+  return (
+    <div
+      id={`node-${row}-${col}`}
+      className={`node ${extraClassName}`}
+      onMouseDown={() => onMouseDown(row, col)}
+      onMouseEnter={() => onMouseEnter(row, col)}
+      onMouseUp={() => onMouseUp()}
+    ></div>
+  );
+};
+
+export default node;
