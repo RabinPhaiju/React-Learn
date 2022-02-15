979b1f57a45e2ba27af576bcf8188d2076ab0aeb
Rabin <rabinphaiju15@gmail.com>
2022-02-15 23:37:54 +0545

    added header

--- src/components/customHook/useMediaQuery/MediaQuery.js
+++ src/components/customHook/useMediaQuery/MediaQuery.js
@@ -1,13 +1,17 @@
 import React from "react"
 import useMediaQuery from "./useMediaQuery"
+import CustomHook from "../../pages/CustomHook"
 import "./MediaQuery.css"
 
 function MediaQuery() {
   const isMobile = useMediaQuery(768)
   const isTablet = useMediaQuery(1024)
   return (
-    <div className={"layout " + (isMobile ? "mobile" : "") + (isTablet ? " tablet" : "")}>
-      <h1>{isMobile ? "Mobile" : isTablet ? "Tablet" : "Desktop"}</h1>
+    <div>
+      <CustomHook />
+      <div className={"layout " + (isMobile ? "mobile" : "") + (isTablet ? " tablet" : "")}>
+        <h1>{isMobile ? "Mobile" : isTablet ? "Tablet" : "Desktop"}</h1>
+      </div>
     </div>
   )
 }
