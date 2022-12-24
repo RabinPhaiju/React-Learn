afca41b65a02970a6196f0d875148149b60693a4
Rabin Phaiju <rabinphaiju15@gmail.com>
2022-12-24 20:35:34 +0545

    convert to typescript

---
+++ src/components/pages/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+
+  const displayName:string = 'About page'
+
+  return (
+    <div className='App'>
+      <h1>{displayName}</h1>
+    </div>
+  )
+}
+
+export default About
